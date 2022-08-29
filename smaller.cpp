744acec92574d02a83c97dae715216388b28a6f2
Will160714 <patrick@LAPTOP-I8OEL6NV.localdomain>
2022-08-29 13:21:54 -0400

    Able to determine the smallest number between 2 numbers

--- smaller.cpp
+++ smaller.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello, World" << std::endl;
+    std::cout << "Enter the first number: ";
+    int first_number;
+    std::cin >> first_number;
+
+    std::cout << "Enter the second number: ";
+    int second_number;
+    std::cin >> second_number;
+
+    std::cout << "" << std::endl;
+
+    if(first_number > second_number){
+        std::cout << "The smaller of the two is " << second_number << std::endl;
+    }
+    else{
+        std::cout << "The smaller of the two is " << first_number << std::endl;
+    }
+    
     return 0;
 }
